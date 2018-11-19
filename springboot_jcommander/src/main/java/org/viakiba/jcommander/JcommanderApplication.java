6711c7505ded24bb3591df8f4f928b166d52dd8d
viakiba <892645423@qq.com>
2018-11-19 17:24:29 +0800

    init jcommander 项目

---
+++ springboot_jcommander/src/main/java/org/viakiba/jcommander/JcommanderApplication.java
@@ -0,0 +1,12 @@
+package org.viakiba.jcommander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcommanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcommanderApplication.class, args);
+	}
+}
