e1ffb36deab2fbe373162b306aca87c7817fa34d
viakiba <892645423@qq.com>
2018-11-20 11:51:08 +0800

    jcommander update

--- springboot_jcommander/src/main/java/org/viakiba/jcommander/JcommanderApplication.java
+++ springboot_jcommander/src/main/java/org/viakiba/jcommander/JcommanderApplication.java
@@ -1,5 +1,6 @@
 package org.viakiba.jcommander;
 
+import com.beust.jcommander.JCommander;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JcommanderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JcommanderApplication.class, args);
+		//处理命令
+		CommandBean commandBean = new CommandBean();
+		JCommander jCommander = new JCommander(commandBean);
+		try{
+			jCommander.setProgramName("JcommanderApplication");
+			jCommander.setAcceptUnknownOptions(true);
+			jCommander.parse(args);
+			System.out.println(commandBean.port);
+		}catch (Exception e){
+			jCommander.usage();
+			e.printStackTrace();
+		}
+		//对参数进行配置
+
+		/**
+		 * 可以重写配置文件 等等
+		 */
+
+		//启动
+		SpringApplication springApplication = new SpringApplication(JcommanderApplication.class);
+		springApplication.run(args);
 	}
 }
